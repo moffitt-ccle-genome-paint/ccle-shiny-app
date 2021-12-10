beccd372114fda7bc75b612b43e244cf102562e2
Tampa, Moffitt Cancer Center, Shaw Immuno Systems Biology Lab <89986836+shawlab-moffitt@users.noreply.github.com>
2021-12-10 14:38:26 -0500

    Adding the Sample Selection App and genome paint html

---
+++ SampleSelectionApp/server_backup.R
@@ -0,0 +1,280 @@
+####----Install and load packages----####
+
+packages <- c("shiny","shinythemes","shinyjqui","pheatmap","RColorBrewer","clusterProfiler",
+              "dplyr","DT","enrichplot","ggplot2","ggpubr","msigdbr","reshape2","tibble","plotly",
+              "readr","limma","enrichR","ggrepel","tidyr","GSVA","tools","shinycssloaders")
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+    install.packages(packages[!installed_packages])
+}
+
+invisible(lapply(packages, library, character.only = TRUE))
+
+####----File Input----####
+
+#Master Meta
+meta_file <- 'MasterMeta_v2.tsv'
+#expression
+expr_file <- 'expression.csv'
+
+
+####----Read Files----####
+
+#read meta
+meta <- as.data.frame(read_tsv(meta_file))
+#read expression
+expr <- as.data.frame(read_csv(expr_file))
+rownames(expr) <- expr[,1]
+expr <- expr[,-1]
+
+
+####----Selection Generation----####
+
+#lineage selection
+#lineage_choice <- "NoneSelected"
+lineage_choice <- unique(meta$lineage)
+#lineage_choice <- c(lineage_choice,"All")
+
+genecols <- colnames(meta)[c(15:ncol(meta))]
+geneList <- "NoneSelected"
+geneList <- c(geneList,gsub("_.*","",genecols))
+
+#disease_choice <- "NoneSelected"
+disease_choice <- unique(meta$primary_disease)
+
+
+
+
+server <- function(input, output, session) {
+    
+    
+    observe({
+        if (input$linordischeck == 1) {
+            output$subsetselec <- renderUI({
+                
+                selectInput("firstChoice","Select Lineage:",choices = lineage_choice,
+                            multiple = F, selected = lineage_choice[1])
+                
+            })
+        }
+        if (input$linordischeck == 2) {
+            output$subsetselec <- renderUI({
+                
+                selectInput("firstChoice","Select Primary Disease:",
+                            choices = disease_choice, multiple = F,
+                            selected = disease_choice[1])
+                
+            })
+        }
+    })
+    
+    observe({
+        if (input$condseleccheck == 5) {
+            output$condselec <- renderUI({
+                
+                selectInput("geneselection","Select Gene of Interest:",
+                            choices = geneList, selected = geneList[1])
+                
+                })
+        }
+    })
+    
+    output$downloadMETA <- downloadHandler(
+        filename = function() {
+            paste(input$metaFileName,".tsv", sep = '')
+        },
+        content = function(file) {
+            
+            firstChoice <- input$firstChoice
+            if (firstChoice %in% lineage_choice) {
+                
+                #subset meta based on lineage
+                meta.u <- meta[which(meta$lineage == firstChoice),]
+                cond <- input$condseleccheck
+                
+                #user chooses gene of interest
+                if (cond == 5){
+                    
+                    if (input$geneselection != geneList[1]) {
+                        gene <- input$geneselection
+                        #assign NULL in case column not found
+                        genecold <- NULL
+                        genecolo <- NULL
+                        genecold <- paste(gene,"_damagingVariant",sep = "")
+                        genecolo <- paste(gene,"_otherVariant",sep = "")
+                        meta_sub <- meta.u[,c("DepMap_ID",genecold,genecolo)]
+                        meta_sub[,c(2,3)] <- lapply(meta_sub[,c(2,3)],as.character)
+                        #rename 
+                        #if (ncol(meta_sub) == 3) {
+                        meta_sub2 <- meta_sub %>%
+                            mutate(Condition = case_when(
+                                meta_sub[,2] == "FALSE" & meta_sub[,3] == "FALSE" ~ "WT",
+                                meta_sub[,2] == "FALSE" & meta_sub[,3] == "TRUE" ~ "NonDamagingVariant",
+                                meta_sub[,2] == "TRUE" & meta_sub[,3] == "FALSE" ~ "DamagingVariant"
+                            ))
+                        #}
+                        meta_sub3 <- meta_sub2[,c(1,4)]
+                        colnames(meta_sub3)[1] <- "SampleName"
+                        meta_sub <- meta_sub3
+                    }
+                    
+                }
+                
+                #user chooses sex condition
+                if (cond == 1) {
+                    
+                    meta.u2 <- meta.u %>%
+                        select(DepMap_ID,sex)
+                    colnames(meta.u2) <- c("SampleName","Condition")
+                    meta_sub <- meta.u2
+                    
+                }
+                ##user chooses sample source
+                #if (cond == 2) {
+                #    
+                #    meta.u2 <- meta.u %>%
+                #        select(DepMap_ID,source)
+                #    colnames(meta.u2) <- c("SampleName","Condition")
+                #    meta_sub <- meta.u2
+                #    
+                #}
+                #user chooses Sample Collection Site
+                if (cond == 3) {
+                    
+                    meta.u2 <- meta.u %>%
+                        select(DepMap_ID,sample_collection_site)
+                    colnames(meta.u2) <- c("SampleName","Condition")
+                    meta_sub <- meta.u2
+                    
+                }
+                #user chooses Primary or metastasis
+                if (cond == 4) {
+                    
+                    meta.u2 <- meta.u %>%
+                        select(DepMap_ID,primary_or_metastasis)
+                    colnames(meta.u2) <- c("SampleName","Condition")
+                    meta_sub <- meta.u2
+                    
+                }
+                
+            }
+            
+            if (firstChoice %in% disease_choice) {
+                
+                meta.u <- meta[which(meta$primary_disease == firstChoice),]
+                cond <- input$condseleccheck
+                
+                #if user chooses gene of interest
+                if (cond == 5) {
+                    
+                    if (input$geneselection != geneList[1]) {
+                        gene <- input$geneselection
+                        #assign NULL in case column not found
+                        genecold <- NULL
+                        genecolo <- NULL
+                        genecold <- paste(gene,"_damagingVariant",sep = "")
+                        genecolo <- paste(gene,"_otherVariant",sep = "")
+                        meta_sub <- meta.u[,c("DepMap_ID",genecold,genecolo)]
+                        meta_sub[,c(2,3)] <- lapply(meta_sub[,c(2,3)],as.character)
+                        #rename 
+                        #if (ncol(meta_sub) == 3) {
+                        meta_sub2 <- meta_sub %>%
+                            mutate(Condition = case_when(
+                                meta_sub[,2] == "FALSE" & meta_sub[,3] == "FALSE" ~ "WT",
+                                meta_sub[,2] == "FALSE" & meta_sub[,3] == "TRUE" ~ "NonDamagingVariant",
+                                meta_sub[,2] == "TRUE" & meta_sub[,3] == "FALSE" ~ "DamagingVariant"
+                            ))
+                        #}
+                        meta_sub3 <- meta_sub2[,c(1,4)]
+                        colnames(meta_sub3)[1] <- "SampleName"
+                        meta_sub <- meta_sub3
+                    
+                    }
+                }
+                #user chooses sex condition
+                if (cond == 1) {
+                    
+                    meta.u2 <- meta.u %>%
+                        select(DepMap_ID,sex)
+                    colnames(meta.u2) <- c("SampleName","Condition")
+                    meta_sub <- meta.u2
+                    
+                }
+                ##user chooses sample source
+                #if (cond == 2) {
+                #    
+                #    meta.u2 <- meta.u %>%
+                #        select(DepMap_ID,source)
+                #    colnames(meta.u2) <- c("SampleName","Condition")
+                #    meta_sub <- meta.u2
+                #    
+                #}
+                #user chooses Sample Collection Site
+                if (cond == 3) {
+                    
+                    meta.u2 <- meta.u %>%
+                        select(DepMap_ID,sample_collection_site)
+                    colnames(meta.u2) <- c("SampleName","Condition")
+                    meta_sub <- meta.u2
+                    
+                }
+                #user chooses Primary or metastasis
+                if (cond == 4) {
+                    
+                    meta.u2 <- meta.u %>%
+                        select(DepMap_ID,primary_or_metastasis)
+                    colnames(meta.u2) <- c("SampleName","Condition")
+                    meta_sub <- meta.u2
+                    
+                }
+                
+            }
+            
+            write_tsv(meta_sub,file)
+        }
+    )
+    
+    output$downloadEXPR <- downloadHandler(
+        filename = function() {
+            paste(input$exprFileName,".tsv", sep = '')
+        },
+        content = function(file) {
+            
+            firstChoice <- input$firstChoice
+            
+            if (firstChoice %in% disease_choice) {
+                
+                samples <- unlist(meta[which(meta$primary_disease == firstChoice),1], use.names = F)
+                expr_sub <- expr[,which(colnames(expr) %in% samples)]
+                expr_sub$gene <- rownames(expr_sub)
+                expr_sub <- expr_sub %>%
+                    relocate(gene)
+            }
+            if (firstChoice %in% lineage_choice) {
+                
+                samples <- unlist(meta[which(meta$lineage == firstChoice),1], use.names = F)
+                expr_sub <- expr[,which(colnames(expr) %in% samples)]
+                expr_sub$gene <- rownames(expr_sub)
+                expr_sub <- expr_sub %>%
+                    relocate(gene)
+            }
+            write_tsv(expr_sub,file)
+            
+        }
+    )
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ SampleSelectionApp/ui.R
@@ -0,0 +1,103 @@
+####----Install and load packages----####
+
+packages <- c("shiny","shinythemes","shinyjqui","dplyr","DT","reshape2",
+              "readr","tidyr","tools","shinycssloaders","stringr")
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+    install.packages(packages[!installed_packages])
+}
+
+invisible(lapply(packages, library, character.only = TRUE))
+
+
+####----File Input----####
+
+#Master Meta
+meta_file <- 'MasterMeta_v2.tsv'
+#expression
+expr_file <- 'expression.csv'
+
+
+####----Read Files----####
+
+#read meta
+meta <- as.data.frame(read_tsv(meta_file))
+#read expression
+expr <- as.data.frame(read_csv(expr_file))
+rownames(expr) <- expr[,1]
+expr <- expr[,-1]
+
+####----Selection Generation----####
+
+#lineage selection
+lineage_choice <- "NoneSelected"
+lineage_choice <- c(lineage_choice, unique(meta$lineage))
+#lineage_choice <- c(lineage_choice,"All")
+
+genecols <- colnames(meta)[c(12:ncol(meta))]
+geneList <- "NoneSelected"
+geneList <- c(geneList,gsub("_.*","",genecols))
+
+disease_choice <- "NoneSelected"
+disease_choice <- c(disease_choice, unique(meta$primary_disease))
+
+
+
+
+####----UI----####
+
+ui <- 
+    
+    navbarPage("CCLE Genome Browser and Expression Analysis Tool",
+               tabPanel("Sample Selection",
+                   fluidPage(
+                       mainPanel(
+                           fluidRow(
+                               column(6,
+                                      h4("Subset CCLE Samples by Lineage or Disease type:"),
+                                      radioButtons("linordischeck","",
+                                                         choices = list("Lineage" = 1,
+                                                                        "Disease Type" = 2)),
+                                      uiOutput("subsetselec"),
+                                      uiOutput("sublinselec")
+                                      ),
+                               column(6,
+                                      h4("Condition Selection:"),
+                                      radioButtons("condseleccheck","",
+                                                         choices = list("Sex" = 1,
+                                                                        #"Sample Source" = 2,
+                                                                        "Sample Collection Site" = 3,
+                                                                        "Primary or Metastasis" = 4,
+                                                                        "Choose Gene of Interest" = 5)),
+                                      uiOutput("condselec")
+                                      )
+                           ),
+                           fluidRow(
+                               column(6,
+                                      textInput("exprFileName","Expression File Download Name:",
+                                                value = "ExpressionData"),
+                                      downloadButton("downloadEXPR","Download Expression Matrix")),
+                               column(6,
+                                      textInput("metaFileName","Meta File Download Name:",
+                                                value = "MetaData"),
+                                      downloadButton("downloadMETA","Download Meta Data")
+                               )
+                                      
+                           )
+                       )
+                       )
+                   )
+               )
+
+
+
+
+
+
+
+
+
+
+
+
+
